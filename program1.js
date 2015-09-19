e9670be4cda83f8dd50bbc2703ca835761edb4dd
ertech <ertech@sun.ertech.net>
2015-09-19 04:56:33 -0500

    fixed problem 2

--- program1.js
+++ program1.js
@@ -1,13 +1,5 @@
 // Program 1
 
-//var num1Str = process.argv[2];
-//var num2Str = process.argv[3];
-//
-//if (num1Str != "" && num2Str != "") {
-//	result = Number(num1Str) + Number(num2Str);
-//} else {
-//	result = "";
-//}
 var result = 0;
 for (i = 2; i < process.argv.length; i++) {
 	if (process.argv[i] != "") {
--- program2.js
+++ program2.js
@@ -3,9 +3,10 @@
 var fs = require('fs');
 
 if (process.argv.length == 3) {
-	var buffer = fs.readFileSync(filename);
-	filename = process.argv[2];
-	console.log(filename);
+	var filename = process.argv[2];
+	var buffer = fs.readFileSync(filename, 'utf8');
+	var fileSplitted = buffer.split('\n');
+	console.log(fileSplitted.length - 1);
 } else {
 	console.log("Usage: program2.js <path_to_file>");
 }
