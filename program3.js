169304226d262ec8f07616bb3b7d0eb7189c99c0
Juan P Daza P <juan.daza@er-technology.net>
2015-09-19 05:47:43 -0500

    Added program3

---
+++ program3.js
@@ -0,0 +1,14 @@
+// program2.js
+
+var fs = require('fs');
+
+if (process.argv.length == 3) {
+	var filename = process.argv[2];
+	var buffer = fs.readFile(filename, 'utf8', function(err, data){
+		if (err) throw err;
+		var fileSplitted = data.split('\n');
+		console.log(fileSplitted.length - 1);
+	});
+} else {
+	console.log("Usage: program2.js <path_to_file>");
+}
