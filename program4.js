28cdaca44201850edef8db53d240c277b5f77b53
Juan P Daza P <juan.daza@er-technology.net>
2015-09-19 06:30:00 -0500

    Added program 4, filtered LS

---
+++ program4.js
@@ -0,0 +1,19 @@
+// program2.js
+
+var fs = require('fs');
+var path = require('path');
+
+if (process.argv.length == 4) {
+	var filepath = process.argv[2];
+	var extension = "." + process.argv[3];
+	fs.readdir(filepath, function(err, list){
+		if (err) throw err;
+		for (i = 0; i < list.length; i++) {
+			if (path.extname(list[i]) === extension) {
+				console.log(list[i]);
+			}
+		}
+	});
+} else {
+	console.log("Usage: program4.js <path_to_file>");
+}
